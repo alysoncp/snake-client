3eae80b10ab9af349456cef35cbcd663f9990518
alysoncp <alyson.c.pickett@gmail.com>
2020-11-04 20:25:28 +0000

    added input.js file

---
+++ input.js
@@ -0,0 +1,17 @@
+// Set up keyboard input
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (data) => {
+    if (data === '\u0003') {
+      process.exit();
+    } else {
+      console.log("Things");
+    }
+  });
+  return stdin;
+}
+
+module.exports = setupInput;
