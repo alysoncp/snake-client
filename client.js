1a10ff7150d96f1f7d2e3b6e128ac180fc9e2f32
alysoncp <alyson.c.pickett@gmail.com>
2020-11-04 19:04:28 +0000

    streamlined data received

--- client.js
+++ client.js
@@ -7,14 +7,6 @@ const connect = function() {
   const conn = net.createConnection({ 
     host: 'localhost',
     port: 50541,
-    /*onread: {
-      // Reuses a 4KiB Buffer for every read from the socket.
-      buffer: Buffer.alloc(4 * 1024),
-      callback: function(nread, buf) {
-        // Received data is available in `buf` from 0 to `nread`.
-        console.log(buf.toString('utf8', 0, nread));
-      }
-    }*/
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
