358396e9f4dc2433b7695f53ac43560b985e6f30
alysoncp <alyson.c.pickett@gmail.com>
2020-11-04 19:16:02 +0000

    added name to client.js

--- client.js
+++ client.js
@@ -10,6 +10,10 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
+  conn.on("connect", () => {
+    console.log("Successful connection!")
+    conn.write('Name: ACP');
+  })
   conn.on("data", (data) => {
     console.log(data);
   })
